9aeb2d67ab3a12d99eb78e8f1bfcdf5dde73d42b
nichol20 <nick.nogue28@outlook.com>
2023-04-17 00:17:46 -0300

    change title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { OptionsProvider } from '@/contexts/OptionsContext'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <OptionsProvider>
+      <Head>
+        <title>Japanese counters</title>
+      </Head>
       <Component {...pageProps} />
     </OptionsProvider>
   )
