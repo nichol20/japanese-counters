7cf55a160fef1963d1029b2ee89e0c2232e3ebba
nichol20 <nick.nogue28@outlook.com>
2023-04-17 23:31:45 -0300

    change favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <OptionsProvider>
       <Head>
+        <link type="image/png" sizes="32x32" rel="icon" href="/cherry-blossom.png" />
         <title>Japanese counters</title>
       </Head>
       <Component {...pageProps} />
